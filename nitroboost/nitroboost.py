c63de825d86fb1b441b77364573a5174c875fe18
xTeen <62380638+Teen1@users.noreply.github.com>
2020-05-06 12:41:12 +0300

    Update nitroboost.py

--- nitroboost/nitroboost.py
+++ nitroboost/nitroboost.py
@@ -8,8 +8,10 @@ class BoostPlugin(commands.Cog):
     async def on_message(self, message):
 
         print(message.type)
-        if message.type == discord.MessageType.new_member:
-            await message.channel.send("TEST")
+        if message.type == discord.MessageType.premium_guild_subscription:
+            booster = message.mentions[0]
+            embed = discord.Embed(title="Nitro Boost", description=f"{booster.mention}, thank you so much for boosting!", color=0xff0000)
+            await message.channel.send(embed=embed)
             
 def setup(bot):
     bot.add_cog(BoostPlugin(bot))
