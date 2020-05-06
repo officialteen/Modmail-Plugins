7ca1f8b595918a31dc6316cb9b8c46c816a4a08a
xTeen <62380638+Teen1@users.noreply.github.com>
2020-05-06 03:19:09 +0300

    Create nitroboost.py

---
+++ nitroboost/nitroboost.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+class BoostPlugin(commands.Cog):
+    def __init__ (self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+
+        if message.author.bot:
+            return
+
+        if "test" in message.content.lower():
+        if message.channel.id == 586645529778454530:
+          await ctx.send("TEST")
+
+def setup(bot):
+    bot.add_cog(BoostPlugin(bot))
