1e27656ac799e962373fd2795f98a6185255f2d6
xTeen <62380638+Teen1@users.noreply.github.com>
2020-05-08 15:06:46 +0300

    Update RC.py

--- RC/RC.py
+++ RC/RC.py
@@ -40,15 +40,15 @@ class RC(commands.Cog):
         )
         await ctx.send(embed=embed)
         
-@rc_f.error
-async def rc_f_error(self, ctx, error):
-    if isinstance(error, commands.MissingPermissions):
-        embed = discord.Embed(
-                title = "Fail",
-                description = "You do not have permissions to recruit players",
-                color=0xff0000
-            )
-        await ctx.send(embed=embed)
+    @rc_f.error
+    async def rc_f_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            embed = discord.Embed(
+                    title = "Fail",
+                    description = "You do not have permissions to recruit players",
+                    color=0xff0000
+                )
+            await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(RC(bot))
