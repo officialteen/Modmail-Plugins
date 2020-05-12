b4574b315dfad37c0048363763b785434f331538
xTeen <62380638+Teen1@users.noreply.github.com>
2020-05-12 16:13:36 +0300

    Create pingapple.py

---
+++ pingapple/pingapple.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+class PingApplePlugin(commands.Cog):
+    def __init__ (self, bot):
+        self.bot = bot
+        self.blurple = 0xFF2B2B
+
+    @commands.command()
+    async def pingapple():
+        if ctx.channel.id == 679741078630563842
+            await ctx.send(f"<@239357569125187588>")
+            await ctx.send(f"<@239357569125187588>")
+            await ctx.send(f"<@239357569125187588>")
+
+    @pingapple.error
+    async def pingapple_error(ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            embed=discord.Embed(
+                title = "Error"
+                description = f"This command can only be used in <#679741078630563842>"
+                color = self.blurple
+            )
+            await ctx.send(embed=embed)
+            await ctxmessage.delete()
+
+def setup(bot):
+    bot.add_cog(PingApplePlugin(bot))
