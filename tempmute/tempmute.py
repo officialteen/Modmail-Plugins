136d8a6da8699f9ebd08e70bdf5693cf07d57f56
xTeen <62380638+Teen1@users.noreply.github.com>
2020-05-22 23:07:11 +0300

    Update tempmute.py

--- tempmute/tempmute.py
+++ tempmute/tempmute.py
@@ -27,7 +27,6 @@ class TimeConverter(commands.Converter):
 class MuteCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
     @commands.command()
     async def tempmute(self, ctx, member:discord.Member, *, time:TimeConverter = None):
         """Mutes a member for the specified time- time in 2d 10h 3m 2s format ex:
