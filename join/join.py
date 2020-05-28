8bfbf880024c0992c72ddef2ac437a8defef0171
xTeen <62380638+Teen1@users.noreply.github.com>
2020-05-28 22:01:14 +0100

    Create join.py

---
+++ join/join.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+class JoinHope(commands.Bot):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+        if "how join hope" in message.content.lower():
+            embed = discord.Embed(description="If you are looking to join Team Hope, make sure to check out the <#681999546028392639 > channel! Please watch the video relating to what team you want to join: Valorant, COD, or Fortnite.\n\nHere are some extra tips to help you join! `1` Use our tags (#Hope8KRC, #HopeOnTop, #HopeWZRC, #HopeVLTRC)\n`2` Be active in our discord server! This is one of the best ways to get recognised!  Good luck grinding!",color=0x06c9ff)
+            embed.set_author(name="How to join Team Hope",icon_url="https://cdn.discordapp.com/attachments/639774534639288330/710608697906167908/Hope_new_bot_logo_blue.jpg")
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/639774534639288330/710609985221951558/extra-bot-logo2.png")
+            await message.author.send(embed=emebd)
+            embed = discord.Embed(
+                description= "You have been sent a DM on how to join Team Hope!"
+            )
+            await message.author.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(JoinHope(bot))
