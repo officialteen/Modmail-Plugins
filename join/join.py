46eece14dcf05b6089105a4533a70a7f82c328ea
xTeen <62380638+Teen1@users.noreply.github.com>
2020-05-29 13:50:16 +0300

    Update join.py

--- join/join.py
+++ join/join.py
@@ -10,7 +10,7 @@ class JoinHope(commands.Cog):
         if message.author.bot:
             return
 
-        if all(word in message.content.lower() for word in ("how", "become", "staff")):
+        if all(word in message.content.lower() for word in ("how", "join", "hope")):
             embed = discord.Embed(description="If you are looking to join Team Hope, make sure to check out the <#681999546028392639> channel! Please watch the video relating to what team you want to join: Valorant, COD, or Fortnite.\n\nHere are some extra tips to help you join!\n`1` Use our tags (#Hope8KRC, #HopeOnTop, #HopeWZRC, #HopeVLTRC)\n`2` Be active in our discord server! This is one of the best ways to get recognised!  Good luck grinding!",color=0x06c9ff)
             embed.set_author(name="How to join Team Hope",icon_url="https://cdn.discordapp.com/attachments/639774534639288330/710608697906167908/Hope_new_bot_logo_blue.jpg")
             embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/639774534639288330/710609985221951558/extra-bot-logo2.png")
