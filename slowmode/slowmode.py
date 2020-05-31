f12eb8fc1158de5fcec94af966b09fc0c4be37fe
xTeen <62380638+Teen1@users.noreply.github.com>
2020-05-31 20:23:25 +0300

    Update slowmode.py

--- slowmode/slowmode.py
+++ slowmode/slowmode.py
@@ -8,7 +8,7 @@ class SlowMode(commands.Cog):
 
     @commands.command()
     async def slowmode(self, ctx, time, channel: discord.TextChannel = None):
-        """Apply a given slowmode time to a channel
+        """Set a slowmode to a channel
         It is not possible to set a slowmode longer than 6 hours
         """
         if not channel:
@@ -23,16 +23,16 @@ class SlowMode(commands.Cog):
         seconds = 0
         match = re.findall("([0-9]+[smhd])", time)
         if not match:
-            return await ctx.send("<:logo:642027408001597460> I don't understand your time format.")
+            return await ctx.send("⚠ I don't understand your time format.")
         for item in match:
             seconds += int(item[:-1]) * units[item[-1]]
         if seconds > 21600:
-            return await ctx.send("<:logo:642027408001597460> You can't slowmode a channel for longer than 6 hours!")
+            return await ctx.send("⚠ You can't slowmode a channel for longer than 6 hours!")
         try:
             await channel.edit(slowmode_delay=seconds)
         except discord.errors.Forbidden:
             return await ctx.send("<:logo:642027408001597460> I don't have permission to do this.")
-        embed=discord.Embed(title="🕙 **Slow Mode**", description=f"{ctx.author.mention} set a slowmode delay of {time} ({seconds}) in {ctx.channel.mention}", color=0xffbb00)
+        embed=discord.Embed(description=f"{ctx.author.mention} set a slowmode delay of {time} in {ctx.channel.mention}", color=0x06c9ff)
         embed.set_author(name="Slow Mode - Team Hope", icon_url="https://cdn.discordapp.com/attachments/639774534639288330/710608697906167908/Hope_new_bot_logo_blue.jpg")
         embed.set_footer(text=" | Made By xTeen#3011")
         await ctx.send(embed=embed)
