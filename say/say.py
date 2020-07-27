f1cfbf1da7b6bdf7119bd086ec95f8e349ecf268
xTeen <62380638+Teen1@users.noreply.github.com>
2020-07-27 11:51:08 +0100

    Delete the initial message

--- say/say.py
+++ say/say.py
@@ -8,6 +8,7 @@ class Say(commands.Cog):
     @commands.command()
     async def say(self, ctx, *, message):
         """Make the bot say something"""
+        await ctx.message.delete()
         await ctx.send(message.replace("@everyone", "@\u200beveryone").replace("@here", "@\u200bhere"))
 
 def setup(bot):
