dd83d2343890f6e1579b86b30877a26e17db3956
xTeen <62380638+Teen1@users.noreply.github.com>
2020-08-29 16:45:55 +0100

    Update statusTest.py

--- statusTest/statusTest.py
+++ statusTest/statusTest.py
@@ -6,11 +6,12 @@ class Test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.Cog.listener()
-    async def on_plugin_ready(self):
+    @commands.command()
+    async def change_status_start(self, ctx):
         await self.bot.change_presence(activity=discord.Game(name="DM For Help | StormServicing"))
         await asyncio.sleep(5)
         await self.bot.change_presence(activity=discord.Game(name="DM For Purchases | StormServicing"))
+        await ctx.send("Done")
     
 def setup(bot):
     bot.add_cog(Test(bot))
