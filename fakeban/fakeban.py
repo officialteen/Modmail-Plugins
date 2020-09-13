7794159163871b628bb59b2ff095c8964e5b9f39
xTeen <62380638+Teen1@users.noreply.github.com>
2020-09-13 13:36:23 +0100

    Create fakeban.py

---
+++ fakeban/fakeban.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+class fakeBan(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.color = self.bot.main_color
+        
+    @commands.command(name="pban")
+    async def fban(self, ctx, member: discord.Member = None, reason = None):
+        await ctx.message.delete()
+        if not member:
+            embed = discord.Embed(title="Error", description="Please provide a user to ban!", color=self.bot.error_color)
+            await ctx.send(embed=embed)
+        else:
+            if not reason:
+                embed = discord.Embed(title="Ban", description=f"{member.mention} Has been banned!", color=self.color)
+                await ctx.send(embed=embed)
+            else:
+                embed = discord.Embed(title="Ban", description=f"{member.mention} Has been banned for {reason}!", color=self.color)
+                await ctx.send(embed=embed)           
+        
+def setup(bot):
+    bot.add_cog(fakeBan(bot))
