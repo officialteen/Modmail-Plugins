aa8ed2d36e9133bdb5ecd45da8e44d9500ca6a93
xTeen <62380638+Teen1@users.noreply.github.com>
2023-02-17 06:02:23 +0300

    Update sudo.py

--- sudo/sudo.py
+++ sudo/sudo.py
@@ -14,7 +14,7 @@ class SudoBot(commands.Cog):
         for webhook in webhooks:
             await webhook.delete()
         webhook = await ctx.channel.create_webhook(name=member.name)
-        await webhook.send(str(shit), username=member.name, avatar_url=member.avatar_url)
+        await webhook.send(str(shit), username=member.name, avatar_url=member.avatar)
         
 async def setup(bot):
     await bot.add_cog(SudoBot(bot))
