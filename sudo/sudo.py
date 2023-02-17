32cd1da015a599203540421104476ad697ee2c6e
xTeen <62380638+Teen1@users.noreply.github.com>
2023-02-17 05:57:03 +0300

    Update sudo.py

--- sudo/sudo.py
+++ sudo/sudo.py
@@ -1,20 +1,20 @@
 import discord
 from discord.ext import commands
 
-class Sudo(commands.Cog):
+class SudoBot(commands.Cog):
     """Sudo plugin by teen"""
     
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def sudo(ctx, member: discord.Member, *, message=None):
+    async def sudo(ctx, member: discord.Member, *, shit=None):
         await ctx.message.delete()
         webhooks = await ctx.channel.webhooks()
         for webhook in webhooks:
             await webhook.delete()
         webhook = await ctx.channel.create_webhook(name=member.name)
-        await webhook.send(str(message), username=member.name, avatar_url=member.avatar_url)
+        await webhook.send(str(shit), username=member.name, avatar_url=member.avatar_url)
         
 async def setup(bot):
-    await bot.add_cog(Sudo(bot))
+    await bot.add_cog(SudoBot(bot))
