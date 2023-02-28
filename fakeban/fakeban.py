1029f3b7032d757378dacc607158c9cc507d3385
xTeen <62380638+Teen1@users.noreply.github.com>
2023-03-01 01:34:18 +0300

    Update fakeban.py

--- fakeban/fakeban.py
+++ fakeban/fakeban.py
@@ -6,18 +6,10 @@ class Fakeban(commands.Cog):
         self.bot = bot
         self.color = self.bot.main_color
         
-    @commands.command(name="pban", aliases=['fban'])
+    @commands.command()
     async def fban(self, ctx, member: discord.Member = None, reason = None):
-        if not member:
-            embed = discord.Embed(title="Error", description="Please provide a user to ban!", color=self.bot.error_color)
-            await ctx.send(embed=embed)
-        else:
-            if not reason:
-                embed = discord.Embed(title="Ban", description=f"{member.mention} Has been banned!", color=self.color)
-                await ctx.send(embed=embed)
-            else:
-                embed = discord.Embed(title="Ban", description=f"{member.mention} Has been banned for {reason}!", color=self.color)
-                await ctx.send(embed=embed)           
+        embed = discord.Embed(title="Ban", description=f"{member.mention} Has been banned for {reason}!", color=self.color)
+        await ctx.send(embed=embed)           
         
 def setup(bot):
     bot.add_cog(Fakeban(bot))
